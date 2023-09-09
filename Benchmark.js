07736376da9734816fc5939032b035fc3c62a51e
Brenno MS <144502762+BMbit01@users.noreply.github.com>
2023-09-08 23:46:34 -0300

    Benchmark.js
    Tempo necessário para calcular 100.000.000 de vezes a potência de 2 elevado a 10.

---
+++ Benchmark.js
@@ -0,0 +1,26 @@
+function calcularpotencia() {
+  return Math.sqrt(2**10);
+}
+
+// Número de repetições
+const repetir = 100000000;
+
+// Registrar o tempo inicial
+const inicio = new Date();
+
+// Loop para calcular a potência de 2 elevado a 10 várias vezes
+for (let i = 0; i < repetir; i++) {
+  calcularpotencia(); 
+}
+
+// Registrar o tempo final
+const fim = new Date();
+
+// Calcular o tempo de execucao em milissegundos
+const tempo_execucao = fim - inicio;
+
+// Calcular o tempo médio de execução
+const media_de_execucao = tempo_execucao / repetir;
+
+console.log(`Tempo: ${tempo_execucao} ms`);
+console.log(`Tempo médio de execução: ${media_de_execucao} ms`);
